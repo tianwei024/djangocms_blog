7208727888bb1c0a7c370b15f814e2c7b1261b86
tianwei <tianwei024@gmail.com>
2014-07-30 15:30:12 +0800

    初始化配置url

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -1,12 +1,30 @@
-from django.conf.urls import patterns, include, url
+#coding=utf-8
 
+# from django.conf.urls import patterns, include, url
+#
+# from django.contrib import admin
+# admin.autodiscover()
+#
+# urlpatterns = patterns('',
+#     # Examples:
+#     # url(r'^$', 'myproject.views.home', name='home'),
+#     # url(r'^blog/', include('blog.urls')),
+#
+#     url(r'^admin/', include(admin.site.urls)),
+# )
+
+from django.conf.urls.defaults import *
 from django.contrib import admin
+from django.conf import settings
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'myproject.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+    (r'^admin/', include(admin.site.urls)),
+    url(r'^', include('cms.urls')),
 )
+
+if settings.DEBUG:
+    urlpatterns = patterns('',
+        (r'^' + settings.MEDIA_URL.lstrip('/'), include('appmedia.urls')),
+    ) + urlpatterns
